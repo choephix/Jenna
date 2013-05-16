03542d7d49a08dd970bb2b7741e2f8872f653afe
choephix <i@thechoephix.com>
2013-05-16 09:55:43 +0300

    Create JennaInstance.cs

---
+++ JennaInstance.cs
@@ -0,0 +1,13 @@
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+
+public static class Jenna {
+
+	Jenna() {
+	
+		SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+	
+	}
+
+}
