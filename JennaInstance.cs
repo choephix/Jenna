9247c0fdb3b61c12b075be3a3103e76d3c2647d6
choephix <i@thechoephix.com>
2013-05-16 10:15:37 +0300

    Update JennaInstance.cs

--- JennaInstance.cs
+++ JennaInstance.cs
@@ -3,17 +3,22 @@ using System.Speech.Synthesis;
 using System.Threading;
 
 public static class Jenna {
-
+	
+	protected readonly SpeechRecognitionEngine _recognizer;
+	protected readonly ManualResetEvent _completed; 
+	protected readonly SpeechSynthesizer _synthesizer;
+	
 	Jenna() {
 	
     	 _completed = new ManualResetEvent(false);
-     	
-		SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine()
+		_recognizer = new SpeechRecognitionEngine();
+		_synthesizer = new SpeechSynthesizer();
 		
 		_recognizer.RequestRecognizerUpdate();
 		_recognizer.LoadGrammar(new Grammar(new GrammarBuilder("test")) { Name = "testGrammar" });
 		
 		_recognizer.SpeechRecognized += _recognizer_SpeechRecognized; 
+		_recognizer.SpeechRecognitonRejected += _recognizer_SpeechRecognitionRejected;
 		
 		_recognizer.SetInputToDefaultAudioDevice();
 		
@@ -24,13 +29,33 @@ public static class Jenna {
 	     
 	}
 	
+	void Say( string msg ) {
+		
+		 Console.WriteLine( msg );
+		 _synthesizer.Speak( msg );
+		
+	}
+	
 	void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)	{
 	     if (e.Result.Text == "test") {
-	         Console.WriteLine("The test was successful!");
+	         Say("The test was successful!");
 	     } else 
 	     if (e.Result.Text == "exit") {
-	         _completed.Set();
+	     	_recognizer.Dispose();
+	     	_synthesizer.Dispose();
+	        _completed.Set();
 	     }
 	} 
+	
+	void _recognizer_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e) {
+	   if (e.Result.Alternates.Count == 0){
+	     Say("Speech rejected. No candidate phrases found.");
+	     return;
+	   }
+	   Say("Speech rejected. Did you mean:");
+	   foreach (RecognizedPhrase r in e.Result.Alternates){
+	    Say("    " + r.Text);
+	   }
+	}
 	
 }
